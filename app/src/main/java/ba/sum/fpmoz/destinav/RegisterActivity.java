12e1f6e5d95a5912fba96f30070227e08901e7c2
ivanagalic163 <44434469+ivanagalic163@users.noreply.github.com>
2024-07-03 18:16:33 +0200

    backend - registracija

--- app/src/main/java/ba/sum/fpmoz/destinav/RegisterActivity.java
+++ app/src/main/java/ba/sum/fpmoz/destinav/RegisterActivity.java
@@ -14,6 +14,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import ba.sum.fpmoz.destinav.models.User;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,5 +29,49 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
+    public void registerUser(View view){
+        EditText firstName = findViewById(R.id.first_name);
+        EditText lastName = findViewById(R.id.last_name);
+        EditText email = findViewById(R.id.emailUser);
+        EditText password = findViewById(R.id.passwordUser);
 
+        String name = firstName.getText().toString();
+        String surname = lastName.getText().toString();
+        String emailUser = email.getText().toString();
+        String passwordUser = password.getText().toString();
+
+        if(name.isEmpty() || surname.isEmpty() || emailUser.isEmpty() || passwordUser.isEmpty()){
+            Toast.makeText(this, "All input fields are required", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            firebaseAuth.createUserWithEmailAndPassword(emailUser,passwordUser).addOnCompleteListener(this,task->{
+                if(task.isSuccessful()){
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    String role = "user";
+                    User member = new User(name,surname,emailUser,passwordUser, role);
+                    String userId = user.getUid();
+
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users");
+                    databaseReference.child(user.getUid()).setValue(member);
+                    Toast.makeText(this, "Registration successful. Thank you!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    String error = task.getException().getMessage();
+                    Log.e("RegisterActivity", "Registration failed" + task.getException().getMessage());
+                    Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+    public void goLogin(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+   // public void goChange(View view){
+       // Intent intent = new Intent(this, ForgotPassword.class);
+      //  startActivity(intent);
+   // }
 }
