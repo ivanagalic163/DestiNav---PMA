6844fa648fa9c6eb35de225390a9ef4f277e19ef
ivanagalic163 <44434469+ivanagalic163@users.noreply.github.com>
2024-07-03 18:40:16 +0200

    napravljen mali dio backenda u home aktivnosti

--- app/src/main/java/ba/sum/fpmoz/destinav/HomeActivity.java
+++ app/src/main/java/ba/sum/fpmoz/destinav/HomeActivity.java
@@ -2,7 +2,21 @@ package ba.sum.fpmoz.destinav;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +24,29 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        TextView welcomeUser = findViewById(R.id.welcomeUser);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference base = FirebaseDatabase.getInstance().getReference("users");
+
+        if(user != null) {
+            String userId = user.getUid();
+            base.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String firstName = snapshot.child("firstName").getValue(String.class);
+                    welcomeUser.setText("Welcome " + firstName);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                   // signOut(null);
+                }
+            });
+            welcomeUser.setText("Welcome " + user.getEmail());
+        }
+
     }
+
+
 }
