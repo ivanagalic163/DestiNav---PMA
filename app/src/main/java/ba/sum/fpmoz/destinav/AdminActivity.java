fe1da9ec423e142872c7fe5654ed6a22648255b1
ivanagalic163 <44434469+ivanagalic163@users.noreply.github.com>
2024-07-03 19:06:48 +0200

    backend - administracija

--- app/src/main/java/ba/sum/fpmoz/destinav/AdminActivity.java
+++ app/src/main/java/ba/sum/fpmoz/destinav/AdminActivity.java
@@ -1,4 +1,191 @@
 package ba.sum.fpmoz.destinav;
 
-public class AdminActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import ba.sum.fpmoz.destinav.models.Item;
+
+public class AdminActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private DatabaseReference mDatabase;
+    private StorageReference mStorageRef;
+    private Uri mImageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin);
+
+        Spinner spinner = findViewById(R.id.spinner_categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference("uploads");
+
+
+        findViewById(R.id.setImage).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+
+        findViewById(R.id.changePassword).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+    }
+
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+        }
+    }
+
+    private void uploadImage() {
+        if (mImageUri != null) {
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String imageUrl = uri.toString();
+                                    addItem(imageUrl);
+                                    progressDialog.dismiss();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AdminActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%...");
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        String extension = "";
+        String mimeType = getContentResolver().getType(uri);
+        if (mimeType != null) {
+            extension = mimeType.substring(mimeType.lastIndexOf('/') + 1);
+        }
+        return extension;
+    }
+
+    private void addItem(String imageUrl) {
+        EditText nameEditText = findViewById(R.id.item_name);
+        EditText descriptionEditText = findViewById(R.id.item_desc);
+        Spinner categorySpinner = findViewById(R.id.spinner_categories);
+        EditText priceEditText = findViewById(R.id.item_price);
+
+        String name = nameEditText.getText().toString().trim();
+        String description = descriptionEditText.getText().toString().trim();
+        String category = categorySpinner.getSelectedItem().toString();
+        String price = priceEditText.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            nameEditText.setError("Name is required");
+            nameEditText.requestFocus();
+            return;
+        }
+
+        if (description.isEmpty()) {
+            descriptionEditText.setError("Location is required");
+            descriptionEditText.requestFocus();
+            return;
+        }
+
+        if (category.equals("Select a category")) { // Assuming first item is a placeholder
+            Toast.makeText(this, "Category is required", Toast.LENGTH_SHORT).show();
+            categorySpinner.requestFocus();
+            return;
+        }
+
+        if (price.isEmpty()) {
+            priceEditText.setError("Price is required");
+            priceEditText.requestFocus();
+            return;
+        }
+
+
+        String id = mDatabase.child("items").push().getKey();
+        Item newItem = new Item(id, name, description, category, price, imageUrl);
+
+
+
+
+
+        mDatabase.child("items").child(id).setValue(newItem);
+
+        Toast.makeText(this, "Item added", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goHome(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToAccount(View view){
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
