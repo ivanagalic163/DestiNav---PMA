be66e45918e6fa3aefd1a466d698d4e8a6ca75fc
ivanagalic163 <44434469+ivanagalic163@users.noreply.github.com>
2024-07-03 18:21:49 +0200

    home aktivnost

--- app/src/main/java/ba/sum/fpmoz/destinav/HomeActivity.java
+++ app/src/main/java/ba/sum/fpmoz/destinav/HomeActivity.java
@@ -1,4 +1,14 @@
 package ba.sum.fpmoz.destinav;
 
-public class HomeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
 }
